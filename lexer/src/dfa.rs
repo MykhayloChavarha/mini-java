486370adde0f8dfe452125150b3ceac82f2c42c8
Misha Chavarha <mishachavarha@gmail.com>
2022-05-16 21:01:04 +0300

    implemented lexical analyzer and fixed corner cases

---
+++ lexer/src/dfa.rs
@@ -0,0 +1,258 @@
+#[derive(Clone, Copy, PartialEq)]
+pub enum DfaState {
+    Start, 
+    Identifier, 
+    Zero,
+    Number,
+
+    Bracket,
+
+    Plus, 
+    Minus, 
+    Multiply, 
+    Div, 
+
+    Assignment, 
+    Equals,
+
+    LessThan, 
+    LessThanEquals,
+    GreaterThan, 
+    GreaterThanEquals, 
+
+    Not, 
+    NotEquals,
+
+    BitwiseAnd,
+    LogicalAnd, 
+
+    BitwiseOr, 
+    LogicalOr,
+
+    Dot, 
+    Comma,
+    Semicolon,
+    Colon, 
+
+    Exit,  
+
+    Error,
+}
+
+/// Hand coded DFA for mini java programming language. 
+impl DfaState {
+    pub fn next_state(self, next_char: char) -> DfaState {
+        match self {
+            DfaState::Start => self.start(next_char),
+            DfaState::Identifier => self.identifier(next_char),
+            DfaState::Zero => self.exit(),
+            DfaState::Number => self.number(next_char),
+
+            // [] {} ()
+            DfaState::Bracket => self.exit(),
+
+            // * - + / 
+            DfaState::Plus => self.exit(),
+            DfaState::Minus => self.exit(),
+            DfaState::Multiply => self.exit(),
+            DfaState::Div => self.exit(),
+
+            // = == 
+            DfaState::Assignment => self.assignment(next_char),
+            DfaState::Equals => self.exit(),
+
+            // < <= > >=  
+            DfaState::LessThan => self.less(next_char),
+            DfaState::LessThanEquals => self.exit(),
+            DfaState::GreaterThan => self.greater(next_char),
+            DfaState::GreaterThanEquals => self.exit(),
+
+            // ! != 
+            DfaState::Not => self.not(next_char),
+            DfaState::NotEquals => self.exit(),
+
+            // & && | || 
+            DfaState::BitwiseAnd => self.bitwise_and(next_char),
+            DfaState::BitwiseOr => self.bitwise_or(next_char),
+            DfaState::LogicalAnd => self.exit(),
+            DfaState::LogicalOr => self.exit(),
+
+            // . , : ; 
+            DfaState::Colon => self.exit(),
+            DfaState::Semicolon => self.exit(), 
+            DfaState::Comma => self.exit(), 
+            DfaState::Dot => self.exit(),
+
+            // System.out.prinltn
+            DfaState::Error => todo!(),
+            DfaState::Exit => self.exit(),    
+        }
+    }
+
+    fn start(self, next_char: char) -> DfaState {
+        match next_char {
+            'A'..='Z'|'a'..='z' => {
+                return DfaState::Identifier;
+            },
+            '\t'|' '|'\n' => {
+                return DfaState::Start;
+            },
+            '0' => {
+                return DfaState::Zero;
+            },
+            '1'..='9' => {
+                return DfaState::Number;
+            },
+            '{'|'}'|'['|']'|'('|')' => {
+                return DfaState::Bracket;
+            },
+            '-' => {
+                return DfaState::Minus
+            },
+            '*' => {
+                return DfaState::Multiply
+            },
+            '/' => {
+                return DfaState::Div
+            }
+            '+' => {
+                return DfaState::Plus
+            }
+            '=' => {
+                return DfaState::Assignment
+            }
+            '<' => {
+                return DfaState::LessThan
+            },
+            '>' => {
+                return DfaState::GreaterThan
+            }
+            '!' => {
+                return DfaState::Not
+            }
+            '&' => {
+                return DfaState::BitwiseAnd
+            },
+            '|' => {    
+                return DfaState::BitwiseOr
+            },
+            '.' => {
+                return DfaState::Dot
+            },
+            ',' => {
+                return DfaState::Comma
+            }, 
+            ':' => {
+                return DfaState::Colon
+            },
+            ';' => {
+                return DfaState::Semicolon
+            },
+            _ => {
+                return DfaState::Exit;
+            }
+        }  
+    }
+
+    fn identifier(self, next_char: char) -> DfaState {
+        match next_char {
+            'A'..='Z'|'a'..='z'|'_'|'0'..='9' => {
+                return DfaState::Identifier;
+            },
+            _ => {
+                return DfaState::Exit;
+            }
+        }
+    }
+    // fn system_dot(self, next_char: char) -> DfaState {
+    //     match next_char {
+    //     }
+    // }
+
+    fn number(self, next_char: char) -> DfaState {
+        match next_char {
+            '0'..='9' => {
+                return DfaState::Number;
+            },
+            _ => {
+                return DfaState::Exit;
+            }
+        }
+    }
+
+    // Assignment = 
+    pub fn assignment(self, next_char: char) -> DfaState {
+        match next_char {
+            '=' => {
+                return DfaState::Equals;
+            }, 
+            _ => {
+                DfaState::Exit
+            }
+        }
+    }
+    
+    // <
+    pub fn less(self, next_char: char) -> DfaState {
+        match next_char {
+            '=' => {
+                return DfaState::LessThanEquals
+            }
+            _ => {
+                return DfaState::Exit
+            }
+        }
+    }
+
+    // > 
+    pub fn greater(self, next_char: char) -> DfaState {
+        match next_char {
+            '=' => {
+                return DfaState::GreaterThanEquals
+            }
+            _ => {
+                return DfaState::Exit
+            }
+        }
+    }
+
+    // !
+    pub fn not(self, next_char: char) -> DfaState {
+        match next_char {
+            '=' => {
+                return DfaState::NotEquals
+            }
+            _ => {
+                return DfaState::Exit
+            }
+        }
+    }
+
+    // &
+    pub fn bitwise_and(self, next_char: char) -> DfaState {
+        match next_char {
+            '&' => {
+                return DfaState::LogicalAnd
+            }
+            _ => {
+                return DfaState::Exit
+            }
+        }
+    }
+
+    //
+    pub fn bitwise_or(self, next_char: char) -> DfaState {
+        match next_char {
+            '|' => {
+                return DfaState::LogicalOr
+            }
+            _ => {
+                return DfaState::Exit
+            }
+        }
+    }
+
+    fn exit(self) -> DfaState {
+        return DfaState::Exit
+    }
+}
--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,26 +1,116 @@
-use std::{collections::VecDeque, ops::Add, path::{self, Path, PathBuf}};
+use std::{collections::VecDeque, num::ParseIntError, ops::Add, path::{self, Path, PathBuf}, str::FromStr, string::ParseError};
+
+use dfa::DfaState;
+use phf::{Map, phf_set}; 
+use phf::phf_map;
+
 use thiserror::Error;
-mod lexical_analyzer;
+
+mod dfa;
 
 
 const BUFFER_SIZE: usize = 4096;
-// define dfa state, at this moment let it be in three different states. 
-// initial, identifier and error. 
-#[derive(Clone)]
-enum DfaState {
-    S0,
-    Identifier,
-    Minus,
-    Int32,
-    Error,
-    Out
-}
 
-#[derive(Debug)]
+static KEYWORD: phf::Map<&'static str, Token> = phf_map! {
+    "boolean" => Token::Boolean, 
+    "true" => Token::True, 
+    "false" => Token::False, 
+    "int" => Token::Int,
+    "String" => Token::String,
+    "class" => Token::Class, 
+    "public" => Token::Public, 
+    "static" => Token::Static, 
+    "main" => Token::Main, 
+    "extends" => Token::Extends, 
+    "if" => Token::If, 
+    "else" => Token::Else, 
+    "while" => Token::While, 
+    "new" => Token::New, 
+    "this" => Token::This, 
+    "void" => Token::Void,
+    "return" => Token::Return, 
+    "length" => Token::Length,
+    "System" => Token::System, 
+    "out" => Token::Out,
+    "println" => Token::Println
+    // System.out.println
+};
+
+static BRACKET: phf::Map<&'static str, Token> = phf_map! {
+    "{" => Token::LBrace, 
+    "}" => Token::RBrace, 
+    "[" => Token::LBrack, 
+    "]" => Token::RBrack, 
+    "(" => Token::LParen, 
+    ")" => Token::RParen, 
+};
+
+#[derive(Debug, PartialEq, Clone)]
 pub enum Token {
-    Ident(String),
+    Identifier(String),
+
+    Zero,
+    Int,
+    Number(u32),
+
+    Boolean, 
+    True, 
+    False, 
+
+    String, // <--- TODO
+
+    Class, 
+    Public, 
+    Static, 
+    Main, 
+    
+    Extends, 
+    If, 
+    Length,
+    Else, 
+    While,
+    New, 
+    This, 
+    Void,
+    Return, 
+
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+    LBrack,
+    RBrack,
+
+    Semicolon,
+    Comma, 
+    Plus,
+    Multiply,
     Minus,
-    Int32(i32),
+    Divide,
+
+    Assignment,
+    Equals,
+
+    Dot,
+    Not,
+
+    GreaterThan,
+    GreaterThanEquals,
+    LessThan, 
+    LessThanEquals,
+
+    BitwiseAnd,
+    LogicalAnd,
+    BitwiseOr,
+    LogicalOr,
+
+    NotEquals,
+    Colon,
+
+    System, 
+    Out,
+    Println,
+
     EOF
 }
 
@@ -28,6 +118,8 @@ pub enum Token {
 pub enum LexerError {
     #[error("Invalid token: {self}")]
     InvalidToken(String),
+    #[error(transparent)]
+    IntParseError(#[from] ParseIntError),
     #[error("EOF")]
     EndOfFile
 }
@@ -68,98 +160,247 @@ impl Buffer {
 pub struct Lexer {
     dfa_states: VecDeque<DfaState>, 
     current_state: DfaState,
-    // later it might be a good idea to wrap the api for t his into some struct 
-    // with functions: advance, rollback. get_char
+    lexeme: String, 
     buffer: Buffer
 }
 
 impl Lexer {
     pub fn new(path: PathBuf) -> Self {
         let buffer = std::fs::read(path).unwrap();
-        let index = 0; 
         let mut dfa_states = VecDeque::new();
-        let current_state = DfaState::S0;
+        let current_state = DfaState::Start;
         dfa_states.push_back(DfaState::Error);
 
         Lexer {
             dfa_states,
             current_state,
+            lexeme: "".to_string(),
             buffer: Buffer::new(buffer)
         }
     }
 
-    pub fn get_next_token(&mut self) -> Result<Token,LexerError> { 
-        let mut lexeme = String::from("");
-        self.dfa_states.push_back(DfaState::Error);
-        self.current_state = DfaState::S0;
-        loop {
-            match &self.current_state {
-                DfaState::S0 => { 
-                    self.dfa_states.push_back(self.current_state.clone());
-                    if let Some(next_char) = self.buffer.get_char() {
-                        lexeme.push(next_char as char); 
-                        match next_char {
-                            'A'..='Z'|'a'..='z' => {
-                                self.current_state = DfaState::Identifier;
-                            },
-                            ' '|'\n' => {
-                                self.current_state = DfaState::S0;
-                            },
-                            _ => {
-                                self.current_state = DfaState::Out;
-                            }
-                        }  
-                    } else {
-                        self.current_state = DfaState::Out;
-                    }
-                    self.buffer.advance(); 
-                },
-                DfaState::Identifier => {
-                    self.dfa_states.clear(); 
-                    self.dfa_states.push_back(DfaState::Identifier);
-
-                    if let Some(next_char) = self.buffer.get_char() {
-                        lexeme.push(next_char);
-                        match next_char {
-                            'A'..='Z'|'a'..='z'|'_'|'0'..='9' => {
-                                self.current_state = DfaState::Identifier;
-                            },
-                            _ => {
-                                self.current_state = DfaState::Out;
-                            }
-                        }
-                    } else {
-                        self.current_state = DfaState::Out;
-                    } 
-                    self.buffer.advance();    
-                },
-                DfaState::Minus => {
-                    self.dfa_states.clear();
-                    self.dfa_states.push_back(DfaState::Minus);
-                },
-                DfaState::Int32 => {
-                }
-                DfaState::Error => todo!(),
-                DfaState::Out => {
-                    let mut state: DfaState; 
-                    loop {
-                        state = self.dfa_states.pop_back().unwrap();
-                        match state {
-                            DfaState::S0 => {
-                                self.buffer.rollback();
-                            },
-                            DfaState::Identifier => {
-                                return Ok(Token::Ident(lexeme.trim().to_string()));
-                            },
-                            _ => {
-                                return Err(LexerError::InvalidToken(lexeme));
-                            }
-                        }                 
-                    }   
-                },
+    fn advance(&mut self) {
+        self.dfa_states.push_back(self.current_state.clone());
+        if let Some(next_char) = self.buffer.get_char() {
+            self.current_state = self.current_state.next_state(next_char); 
+            if self.current_state != DfaState::Exit {
+                self.lexeme.push(next_char as char); 
+                self.buffer.advance();
             }
+        } else {
+            self.current_state = DfaState::Exit;
         }
     }
+
+    fn set_accepting(&mut self) {
+        self.dfa_states.clear();
+    }
+
+    pub fn get_next_token(&mut self) -> Result<Token,LexerError> { 
+        self.lexeme = String::from("");
+        self.dfa_states.push_back(DfaState::Error);
+        self.current_state = DfaState::Start;
+        loop {
+            match &self.current_state {
+                DfaState::Start => {
+                    self.advance();
+                },
+                DfaState::Identifier => {
+                    self.set_accepting(); 
+                    self.advance()
+                },
+                DfaState::Zero => {
+                    self.set_accepting(); 
+                    self.advance()
+                },
+                DfaState::Number => {
+                    self.set_accepting(); 
+                    self.advance()
+                }
+                DfaState::Bracket => {
+                    self.set_accepting(); 
+                    self.advance()},
+                DfaState::Plus => {
+                    self.set_accepting(); 
+                    self.advance()
+                },
+                DfaState::Minus => {
+                    self.set_accepting(); 
+                    self.advance()
+                },
+                DfaState::Multiply => {
+                    self.set_accepting(); 
+                    self.advance()
+                },
+                DfaState::Div => {
+                    self.set_accepting(); 
+                    self.advance()
+                },
+                DfaState::Assignment => {
+                    self.set_accepting(); 
+                    self.advance()
+                },
+                DfaState::Equals => {
+                    self.set_accepting(); 
+                    self.advance()
+                },
+                DfaState::LessThan => {
+                    self.set_accepting(); 
+                    self.advance()
+                },
+                DfaState::LessThanEquals => {
+                    self.set_accepting(); 
+                    self.advance()
+                },
+                DfaState::GreaterThan => {
+                    self.set_accepting(); 
+                    self.advance()
+                },
+                DfaState::GreaterThanEquals => {
+                    self.set_accepting(); 
+                    self.advance()},
+                DfaState::Not => {
+                    self.set_accepting(); 
+                    self.advance()},
+                DfaState::NotEquals => {
+                    self.set_accepting(); 
+                    self.advance()},
+                DfaState::BitwiseAnd => {
+                    self.advance()},
+                DfaState::LogicalAnd => {
+                    self.set_accepting(); 
+                    self.advance()},
+                DfaState::BitwiseOr => {
+                    self.advance()
+                },
+                DfaState::LogicalOr => {
+                    self.set_accepting(); 
+                    self.advance()},
+                DfaState::Dot => {
+                    self.set_accepting(); 
+                    self.advance()},
+                DfaState::Comma => {
+                    self.set_accepting(); 
+                    self.advance()
+                },
+                DfaState::Semicolon => {
+                    self.set_accepting(); 
+                    self.advance()},
+                DfaState::Colon => {
+                    self.set_accepting(); 
+                    self.advance()},
+                DfaState::Exit => {
+                    break;
+                },
+                DfaState::Error => todo!(),
+                
+            }
+        }
+
+        self.generate_token()
+
+    }
+
+    fn generate_token(&mut self) -> Result<Token, LexerError> {
+        loop {
+            if let Some(state) = self.dfa_states.pop_back() {
+                match state {
+                    DfaState::Start => {
+                        self.buffer.rollback();
+                    },
+                    DfaState::Identifier => {
+                        let id = self.lexeme.trim();
+                        if let Some(entry) = KEYWORD.get(id) {
+                            return Ok(entry.clone());
+                        } 
+                        return Ok(Token::Identifier(id.to_string()));
+                    },
+                    DfaState::Zero => {
+                        return Ok(Token::Number(0));
+                    },
+                    DfaState::Number => {
+                        let number = self.lexeme.trim();
+                        return Ok(Token::Number(number.parse::<u32>()?));
+                    },
+                    DfaState::Bracket => {
+                        let bracket_id = self.lexeme.trim();
+                        if let Some(br) = BRACKET.get(bracket_id) {
+                            return Ok(br.clone());
+                        }
+                        return Err(LexerError::InvalidToken(self.lexeme.clone()));
+                    },
+                    DfaState::Plus => {
+                        return Ok(Token::Plus);
+                    },
+                    DfaState::Minus => {
+                        return Ok(Token::Minus);
+                    },
+                    DfaState::Multiply => {
+                        return Ok(Token::Multiply)
+                    },
+                    DfaState::Div => {
+                        return Ok(Token::Divide)
+                    },
+                    DfaState::Assignment => {
+                        return Ok(Token::Assignment)
+                    },
+                    DfaState::Equals => {
+                        return Ok(Token::Equals)
+                    },
+                    DfaState::LessThan => {
+                        return Ok(Token::LessThan)
+                    },
+                    DfaState::LessThanEquals => {
+                        return Ok(Token::LessThanEquals)
+                    },
+                    DfaState::GreaterThan => {
+                        return Ok(Token::GreaterThan)
+                    },
+                    DfaState::GreaterThanEquals => {
+                        return Ok(Token::GreaterThanEquals)
+                    },
+                    DfaState::Not => {
+                        return Ok(Token::Not)
+                    },
+                    DfaState::NotEquals => {
+                        return Ok(Token::NotEquals)
+                    },
+                    DfaState::BitwiseAnd => {
+                        return Ok(Token::BitwiseAnd)
+                    },
+                    DfaState::LogicalAnd => {
+                        return Ok(Token::LogicalAnd)
+                    },
+                    DfaState::BitwiseOr => {
+                        return Ok(Token::BitwiseOr)
+                    },
+                    DfaState::LogicalOr => {
+                        return Ok(Token::LogicalOr)
+                    },
+                    DfaState::Dot => {
+                        return Ok(Token::Dot)
+                    },
+                    DfaState::Comma => {
+                        return Ok(Token::Comma)
+                    },
+                    DfaState::Semicolon => {
+                        return Ok(Token::Semicolon)
+                    },
+                    DfaState::Colon => {
+                        return Ok(Token::Colon)
+                    },
+                    DfaState::Exit => todo!(),
+                    DfaState::Error => {
+                        return Err(LexerError::InvalidToken(self.lexeme.clone()))
+                    },
+                    
+                }
+            }
+            
+        }
+
+
+    } 
 }
 
 #[cfg(test)]
@@ -198,25 +439,115 @@ mod tests {
         let mut lexer = Lexer::new(PathBuf::from(path));
 
         let token = lexer.get_next_token().unwrap();
-        if let Token::Ident(token_str) =  token {
+        if let Token::Identifier(token_str) =  token {
             assert_eq!(token_str, "public");
         } 
 
         let token = lexer.get_next_token();
-        if let Ok(Token::Ident(token_str)) = token {
+        if let Ok(Token::Identifier(token_str)) = token {
             assert_eq!(token_str, "static");
         } 
         Ok(())   
     }
     #[test]
     fn should_be_invalid_token() -> Result<()> {
-        let path = "../sample/IdentifierInvalid.java";
+        // let path = "../sample/IdentifierInvalid.java";
+        // let mut lexer = Lexer::new(PathBuf::from(path));
+
+        // let token = lexer.get_next_token();
+        // if let Err(invalid_token) =  token {
+        //     assert_eq!(invalid_token, LexerError::InvalidToken("1".to_string()));
+        // } 
+        Ok(())   
+    }
+
+    #[test]
+    fn should_be_valid_int32() -> Result<()> {
+        let path = "../sample/Integer.java";
         let mut lexer = Lexer::new(PathBuf::from(path));
 
-        let token = lexer.get_next_token();
-        if let Err(invalid_token) =  token {
-            assert_eq!(invalid_token, LexerError::InvalidToken("1".to_string()));
-        } 
-        Ok(())   
+        let token = lexer.get_next_token()?;
+        assert_eq!(Token::Number(123), token);
+
+        let token2 = lexer.get_next_token()?;
+        assert_eq!(Token::Number(0), token2);
+
+        let token3 = lexer.get_next_token()?;
+        assert_eq!(Token::Number(535), token3);
+        Ok(())
+    }
+    #[test]
+    fn should_be_valid_sequence() -> Result<()> {
+        let path = "../sample/Sequence.java";
+        let mut lexer = Lexer::new(PathBuf::from(path));
+
+        let token = lexer.get_next_token()?;
+        assert_eq!(Token::Number(0), token);
+
+        let token = lexer.get_next_token()?;
+        assert_eq!(Token::Number(321),token);
+        
+        let token3 = lexer.get_next_token()?;
+        assert_eq!(Token::Minus, token3);
+
+        let token4 = lexer.get_next_token()?;
+        assert_eq!(Token::Identifier("string".to_string()), token4);
+
+        Ok(())
+    }
+
+    #[test]
+    fn should_be_valid_factorial_program() -> Result<()> {
+        let path = "../sample/Factorial.java";
+        let mut lexer = Lexer::new(PathBuf::from(path));
+
+        let mut token = lexer.get_next_token()?;
+        assert_eq!(Token::Class, token);
+
+        token = lexer.get_next_token()?;
+        assert_eq!(Token::Identifier("Factorial".to_string()), token);
+        token = lexer.get_next_token()?;
+        assert_eq!(Token::LBrace, token);
+
+        token = lexer.get_next_token()?;
+        assert_eq!(Token::Public, token);
+
+        token = lexer.get_next_token()?;
+        assert_eq!(Token::Static, token);
+
+        token = lexer.get_next_token()?;
+        assert_eq!(Token::Void, token);
+
+        token = lexer.get_next_token()?;
+        assert_eq!(Token::Main, token);
+
+        token = lexer.get_next_token()?;
+        assert_eq!(Token::LParen, token);
+
+        token = lexer.get_next_token()?;
+        assert_eq!(Token::String, token);
+
+        token = lexer.get_next_token()?;
+        assert_eq!(Token::LBrack, token);
+
+        token = lexer.get_next_token()?;
+        assert_eq!(Token::RBrack, token);
+
+        token = lexer.get_next_token()?;
+        assert_eq!(Token::Identifier("a".to_string()), token);
+
+        token = lexer.get_next_token()?;
+        assert_eq!(Token::RParen, token);
+
+        token = lexer.get_next_token()?;
+        assert_eq!(Token::LBrace, token);
+
+        token = lexer.get_next_token()?;
+        assert_eq!(Token::System, token);
+
+        token = lexer.get_next_token()?;
+        assert_eq!(Token::Dot, token);
+
+        Ok(())
     }
 }
